689847411f18c4188b67c18f0e176efa41cfb829
Antoine Fischini <fischini.antoine@free.fr>
2018-07-27 00:55:30 +0200

    AndroidStudio + OpenCV

---
+++ app/src/main/java/com/example/sudokumate/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sudokumate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV imported");
+        }
+        else{
+            Log.d(TAG, "Error while loading OpenCV");
+        }
+    }
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
